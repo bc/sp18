512919d1f12629acd68d675c5fd7e719a3e52ad2
bc <brian.cohn@usc.edu>
2018-01-25 02:47:52 -0800

    #3 initiated new concept via roll-a-ball example

---
+++ vectorMatch/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	// late update runs every frame, but runs at the end. good for cameras/ the last layer of operations. 
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ vectorMatch/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed;
+	private Vector3 nextPosition;
+	void Update() {
+		rb = GetComponent<Rigidbody>();
+		
+	}
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			Vector3 randomPosition = new Vector3(Random.Range(-10.0f, 10.0f), 0.5f, Random.Range(-10.0f, 10.0f));
+			other.gameObject.transform.position = randomPosition;
+		}
+	}
+}
