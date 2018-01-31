6b4207ebc5b5ee2577fdefca60ecf72b9acfe01b
Nicholas Terrile <nicky@terrile.com>
2018-01-30 17:06:28 -0800

    Created socket connection

--- vectorMatch/Assets/Scripts/PlayerController.cs
+++ vectorMatch/Assets/Scripts/PlayerController.cs
@@ -1,26 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class PlayerController : MonoBehaviour {
+	
 	private Rigidbody rb;
 	public float speed;
 	private Vector3 nextPosition;
+	private socketScript ss;
+	public bool isReady = false;
+	public string text = "";
+
 	void Update() {
 		rb = GetComponent<Rigidbody>();
-		
 	}
+
 	void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce (movement * speed);
 	}
+
 	void OnTriggerEnter(Collider other) 
 	{
 		if (other.gameObject.CompareTag ("Pick Up"))
 		{
 			Vector3 randomPosition = new Vector3(Random.Range(-10.0f, 10.0f), 0.5f, Random.Range(-10.0f, 10.0f));
 			other.gameObject.transform.position = randomPosition;
+			text = randomPosition.x.ToString() + "," + randomPosition.y.ToString() + "," + randomPosition.z.ToString();
+			isReady = true;
 		}
 	}
 }
---
+++ vectorMatch/Assets/Scripts/socketScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System;
+
+public class socketScript : MonoBehaviour {
+
+	private TCPConnection myTCP;
+	private string serverMsg;
+	public string msgToServer;
+	public GameObject player; //must set player in the inspector
+	private PlayerController playerController;
+
+	void Awake() {
+		//add a copy of TCPConnection to this game object
+		myTCP = gameObject.AddComponent<TCPConnection>();
+		playerController = player.GetComponent<PlayerController> ();
+	}
+		
+	void Start () {
+		myTCP.setupSocket ();
+	}
+		
+	void Update() {
+		SocketResponse ();
+		//if connection has not been made, display button to connect
+		if (myTCP.socketReady == false) {
+			myTCP.setupSocket();
+		}
+		//once connection has been made, display editable text field with a button to send that string to the server (see function below)
+		if (myTCP.socketReady == true) {
+			if (playerController.isReady) {
+				SendToServer(playerController.text);
+				playerController.isReady = false;
+			}
+		}
+	}
+		
+	//socket reading script
+	void SocketResponse() {
+		string serverSays = myTCP.readSocket();
+		if (serverSays != "") {
+			Debug.Log("[SERVER]" + serverSays);
+		}
+	}
+		
+	//send message to the server
+	public void SendToServer(string str) {
+		myTCP.writeSocket(str);
+		Debug.Log ("[CLIENT] -> " + str);
+	}
+
+}
