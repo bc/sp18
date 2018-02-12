3ee1932de3b7e2e3dc5b0d0413dc3d1d2d8f8621
Forrest Dunlap <fdunlap@usc.edu>
2018-02-12 00:22:22 -0800

    datasaving script upload

---
+++ vectorMatch/SpuriousDataScripts/BallInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BallInfo
+{
+    public List<float> x = new List<float>();
+    public List<float> z = new List<float>();
+    public List<Int64> timeStamp = new List<Int64>();
+    public int count;
+}
