994fe30124a33b818d35ee47124f9cd913b0f33d
Forrest Dunlap <fdunlap@usc.edu>
2018-03-28 19:59:54 -0700

    Adding Arduino tendon driver

---
+++ TendonDriverCode/ArduinoTendonDriver.ino
@@ -0,0 +1,37 @@
+#include <HX711.h>
+
+int numLoadCells = 7;
+int sckPins[] = {21, 23, 25, 35, 29, 31, 33};
+int datPins[] = {20, 22, 24, 34, 28, 30, 32};
+int calPin = 13;
+
+HX711 loadCell[7];
+
+void setup() {
+  //Serial setup
+  Serial.begin(115200);
+
+  pinMode(calPin, INPUT_PULLUP);
+  
+  for(int i = 0; i < numLoadCells; i++){
+    Serial.println(i);
+      loadCell[i].begin(datPins[i], sckPins[i], 64);
+      loadCell[i].set_scale();
+      loadCell[i].tare();
+  }
+  
+}
+
+float getLoadCellVal(int index){
+    return float(loadCell[index].read());
+}
+
+void loop() {
+  for(int i = 0; i < numLoadCells; i++){
+    Serial.print(getLoadCellVal(i));
+    if(i != numLoadCells - 1){
+    Serial.print(",");
+    }
+  }
+  Serial.print("\n");
+}
