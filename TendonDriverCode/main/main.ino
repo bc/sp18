724c5aeb5b586065dfee56aec9178dd9c079e37c
bc <brian.cohn@usc.edu>
2018-04-09 19:18:11 -0700

    fully documented arduino code and modularized load cell functions #1

--- TendonDriverCode/main/main.ino
+++ TendonDriverCode/main/main.ino
@@ -1,41 +1,51 @@
 #include <HX711.h>
 
 int numLoadCells = 7;
-int sckPins[] = {21, 23, 25, 35, 29, 31, 33};
-int datPins[] = {20, 22, 24, 34, 28, 30, 32};
+//each column represents a loadCell
+int sckPins[] = {25, 23, 35, 33, 31, 29, 21};
+int datPins[] = {24, 22, 34, 32, 30, 28, 20};
+
 int calPin = 13;
 int loadCellGain = 128;
 HX711 loadCell[7];
 
 void setup() {
-  //Serial setup
-  Serial.begin(115200);
+  Serial.begin(115200); //baud rate; must match Python code
   pinMode(calPin, INPUT_PULLUP);
   for(int i = 0; i < numLoadCells; i++){
-      loadCell[i].begin(datPins[i], sckPins[i], loadCellGain);
-      loadCell[i].set_offset(0);
-      loadCell[i].set_scale(0.01);
+    initializeLoadCell(i, datPins[i], sckPins[i], loadCellGain);
   }
 }
 
+// returns the current millivolt level of a given HX711 load amplifier.
 float getLoadCellVal(int index){
     return float(loadCell[index].read());
 }
 
+// begins the load cell, sets the offset + scale to zero.
+// Arduino is not responsible for handling calibration.
+void initializeLoadCell(int loadCellIndex, int datPin, int sckPin, int loadCellGain){
+      loadCell[loadCellIndex].begin(datPin, sckPin, loadCellGain);
+      loadCell[loadCellIndex].set_offset(0);
+      loadCell[loadCellIndex].set_scale(0.01);
+}
+
+// for each of the loadcells in the loadCell array, read the value and make a CSV line.
+// the final value is not followed by a comma.
 String readAndComposeDataLineString(int numLoadCells){
   String dataline = "";
   for(int i = 0; i < numLoadCells; i++){
     dataline += String(getLoadCellVal(i),0);
+    //Append a comma after each load cell value, excluding the final one
     if(i != numLoadCells - 1){
-    dataline += ",";
+      dataline += ",";
     }
   }
-  dataline += "\n";
   return(dataline);
 }
 
 void loop() {
   String myLine = readAndComposeDataLineString(numLoadCells);
-  Serial.print(myLine);
+  Serial.println(myLine);
   delay(7.16);
 }
