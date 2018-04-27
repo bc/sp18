ffb9b0bc23fdc4583aab231e14604881c96b00dd
bc <brian.cohn@usc.edu>
2018-04-27 19:41:19 +0000

    pep8 logcsv

--- TendonDriver/RPICode/logCSV.py
+++ TendonDriver/RPICode/logCSV.py
@@ -1,11 +1,15 @@
 from genericHelperFunctions import *
-def loads_to_logCSVline(logFile,loads):
-	loadCellCSVLine = ",".join([str(x) for x in loads]) + "\n"
-	loadCellWithTime = unixtime_str() + "," + loadCellCSVLine
-	logFile.write(loadCellWithTime)
+
+
+def loads_to_logCSVline(logFile, loads):
+    loadCellCSVLine = ",".join([str(x) for x in loads]) + "\n"
+    loadCellWithTime = unixtime_str() + "," + loadCellCSVLine
+    logFile.write(loadCellWithTime)
+
 
 def initialize_loadcell_log():
-	logFile = open('/home/pi/Downloads/output_logs/log_' + unixtime_str() + '.txt', 'a')
-	commaSeparatedLoadCellCols = ",".join(["l" + str(i) for i in range(7)])
-	logFile.write("unixtime," + commaSeparatedLoadCellCols + "\n")
-	return(logFile)
+    logFile = open('/home/pi/Downloads/output_logs/log_' +
+                   unixtime_str() + '.txt', 'a')
+    commaSeparatedLoadCellCols = ",".join(["l" + str(i) for i in range(7)])
+    logFile.write("unixtime," + commaSeparatedLoadCellCols + "\n")
+    return(logFile)
