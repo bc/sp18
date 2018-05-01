37fe0eb059ce5e479d332436cc6d7c8e244de286
bc <brian.cohn@usc.edu>
2018-04-30 18:17:35 -0700

    pushed up to 100 values between0 and 10

--- TendonDriver/PyController/pub.py
+++ TendonDriver/PyController/pub.py
@@ -18,7 +18,7 @@ socket.bind("tcp://*:%s" % port)
 def publish_loop(initialized_socket, sleep_time_seconds):
     while True:
         topic = b"map"
-        messagedata = np.random.uniform(0.0, 10.0, 7)
+        messagedata = np.random.uniform(0.0, 10.0, 100)
         # print("%d %d" % (topic, messagedata[0]))
         # print(messagedata)
         pickled_contents = pickle.dumps(messagedata)
