10c826d7b2827cab976b6af09cb93f411a6c00c3
sitharakannan <sithara.kamalakkannan@gmail.com>
2018-05-22 20:33:37 -0700

    modified publisher and added subscriber

--- TendonDriver/RPICode/pubstream.py
+++ TendonDriver/RPICode/pubstream.py
@@ -1,5 +1,5 @@
 import zmq
-import cPickle
+import pickle
 import numpy as np
 
 ### Currently just spewing random data but meant to help in Visualising the data sent from Rpi ###
@@ -13,12 +13,25 @@ def instantiate_zmq_publisher(port=12345):
 
 sample_data = np.random.uniform(0.0, 10.0, 100)
 
-def publish_observation(initialized_socket, messagedata=sample_data):
+def publish_observation(init_socket):
     topic = b"map"
-    # print("%d %d" % (topic, messagedata[0]))
-    # print(messagedata)
-    pickled_contents = cPickle.dumps(messagedata)
+    measuredForces = []
+    targetForces = []
+    commands = []
+    measuredForces.append([random.uniform(0,1) for i in range(7)])
+    targetForces.append([random.uniform(0,1) for i in range(7)])
+    commands.append([random.uniform(1,500) for i in range(7)])
+    messagedata = (np.vstack([measuredForces, targetForces, commands]), time.time())
+    #print messagedata #print messagedata[0][0]
+    #print("%d %d" % (topic, messagedata[0]))
+    pickled_contents = pickle.dumps(messagedata)
+    #print "pickled_contents",pickled_contents, type(pickled_contents)
+
     try:
-        initialized_socket.send_multipart([topic, pickled_contents])
+        init_socket.send_multipart([topic, pickled_contents])
+        time.sleep(0.1)
     except Exception:
         print('Issue in publish_observation')
+
+init_socket = instantiate_zmq_publisher()
+publish_observation(init_socket)
