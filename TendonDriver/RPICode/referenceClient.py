b825cd7b6aaf8a5c652d60089bbe157a18214a39
sitharakannan <sithara.kamalakkannan@gmail.com>
2018-06-05 00:53:02 +0000

    #21 updated random refForce structure

--- TendonDriver/RPICode/referenceClient.py
+++ TendonDriver/RPICode/referenceClient.py
@@ -3,18 +3,19 @@ import time
 import random
 import pickle
 
-def server_push(port="5556"):
+def server_push(port="5558"):
     context = zmq.Context()
     socket = context.socket(zmq.REP)
     socket.bind("tcp://*:%s" % port)
     print("Running server on port: ", port)
     while True:
         message = socket.recv()
-        print("Received referenceForce @ %s" % (message).decode("utf-8"))
+        print("Received : %s" % message) #(message).decode("utf-8")
         referenceForces = []
-        referenceForces.append([random.uniform(0,1) for i in range(7)])
+        randomRef = random.uniform(0,1)
+        referenceForces.append([randomRef for i in range(7)])
         pickled_contents = pickle.dumps(referenceForces)
         socket.send(pickled_contents)
-        time.sleep(8)
+        time.sleep(10)
 
 server_push()
