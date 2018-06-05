1bbe1d90b56fab76569b9073b0bfb8c0d8188124
sitharakannan <sithara.kamalakkannan@gmail.com>
2018-06-05 00:59:16 +0000

    #21 changed function name

--- TendonDriver/RPICode/referenceClient.py
+++ TendonDriver/RPICode/referenceClient.py
@@ -3,7 +3,7 @@ import time
 import random
 import pickle
 
-def server_push(port="5558"):
+def push_randomRefForce(port="5558"):
     context = zmq.Context()
     socket = context.socket(zmq.REP)
     socket.bind("tcp://*:%s" % port)
@@ -17,5 +17,3 @@ def server_push(port="5558"):
         pickled_contents = pickle.dumps(referenceForces)
         socket.send(pickled_contents)
         time.sleep(10)
-
-server_push()
